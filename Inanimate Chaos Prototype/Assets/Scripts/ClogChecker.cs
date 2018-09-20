6b60203d88e97eb496f719ac424686105d971c76
KevWang948 <22383703+KevWang948@users.noreply.github.com>
2018-09-20 12:07:40 -0600

    Added Toilet Clogging and Flushing Detection
    Clog the toilet then flush it to win!

---
+++ Inanimate Chaos Prototype/Assets/Scripts/ClogChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClogChecker : MonoBehaviour {
+
+    public bool isClogged = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.name + " Clogged!");
+        isClogged = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log(gameObject.name + " Unclogged!");
+        isClogged = false;
+    }
+}
---
+++ Inanimate Chaos Prototype/Assets/Scripts/FlushToilet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlushToilet : MonoBehaviour {
+
+    public ClogChecker clog;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Flushed the toilet!");
+        if (clog.isClogged) {
+            Debug.Log("You win!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Not Flushing anymore.");
+    }
+}
